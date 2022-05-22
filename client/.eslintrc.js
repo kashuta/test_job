e5c42d42b67a35146212430d5c4a7497e0dc9f64
Artem Kashuta <kashuta@gmail.com>
2022-05-22 18:09:04 +0300

    fix: esLint for react\redux added

---
+++ client/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:react-redux/recommended'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        "react-redux",
+    ],
+    rules: {
+        "react-redux/connect-prefer-named-arguments": 2,
+    },
+};
